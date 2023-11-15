2a74f06c2d569344094aa72bbdb4b8704d54a938
MisterFried <wilbaspeyras@gmail.com>
2023-11-15 14:38:08 +0200

    fix: strict mode render

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import "./styles/styles.scss";
 import Header from "./components/header/header";
 import Sidebar from "./components/sidebar/sidebar";
 import Dashboard from "./components/dashboard/dashboard";
-import { createContext } from "react";
+import { StrictMode, createContext } from "react";
 import { GlobalUserData } from "./types/interfaces";
 
 export const UserDataContext = createContext<undefined | GlobalUserData>(undefined);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<>
+	<StrictMode>
 		<Header />
 		<Sidebar />
 		<Dashboard />
-	</>
+	</StrictMode>
 );
